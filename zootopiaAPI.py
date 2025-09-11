2c1cf453641dc4beeb8be39d058a33f7ed7c764c
Zahra Rauf <zahra.aleem@gmail.com>
2025-09-11 16:22:51 +0200

    load_API_data function created to fetch data from API

--- zootopiaAPI.py
+++ zootopiaAPI.py
@@ -1,3 +1,18 @@
+import requests
+import json
 
-
+def load_API_date():
+    """
+    This function reads data from an API and loads through the get function in
+    requests module.
+    """
+    name = 'fox'
+    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
+    response = requests.get(api_url, headers={'X-Api-Key': 'Gyfnk0yFcglo+i9JYnYu6Q==0gk5md4xfMsblhcH'})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        #print(json.dumps(data, indent=4, sort_keys=True))
+        return data
+    else:
+        return "Error:", response.status_code, response.text
 
